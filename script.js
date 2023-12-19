2ece2ee216b3cd0a8b7045497ce990cd9d638afb
Ola-Reda <ola.reda.900@gmail.com>
2023-12-19 11:52:47 +0200

    Add js project

---
+++ script.js
@@ -0,0 +1,6 @@
+const simpleCode = document.querySelector('.simple')
+const hexCode = document.querySelector('.hex')
+const main = document.querySelector('main')
+const color = document.querySelector('.color')
+
+console.log(simpleCode, hexCode, main, color)
